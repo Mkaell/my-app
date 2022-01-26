9ba61cea9df506376afb906d29d07b99395de915
ypetukhov <yury.petukhov@PETUKHOV-MBP.local>
2022-01-26 13:42:09 +0300

    lesson_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const header = (
+  <header>Hello my first app</header>
+)
+
+const footer = (
+  <footer>Hello my first footer</footer>
+)
+interface IHeaderProps {
+  appTitle: string,
+  color: string
+}
+
+interface IMain {
+  login: boolean
+}
+
+const Main = ({login}: IMain) => {
+  return (
+    <main>
+      {
+        login ? 'You can see this content' : 'Go away'
+      }
+    </main>
+  )
+}
+
+const stylesOfHeader = {
+  color:'red'
+};
+
+const Header = ({appTitle, color}: IHeaderProps) => {
+  
+  return (
+     <header style={stylesOfHeader}>
+       Hello {appTitle === 'first' ? 'first' : 'second'}     
+     </header>
+     )
+} // компанент 
 function App() {
+   const [login, setLogin] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header appTitle={'first'} color={'blue'} />
+      <Main login={login} />
+
+      <Header appTitle={'second'} color={'green'}/> 
+      <button onClick={() => setLogin(!login)}>Login</button>
     </div>
   );
 }
